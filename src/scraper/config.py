3c2dbc867fc67ac4ac5b3e5deb45a6cc8563d28f
Simon Wiles <simonjwiles@gmail.com>
2024-08-31 09:26:51 -0700

    Add support for CSU Dominguez Hills

--- src/scraper/config.py
+++ src/scraper/config.py
@@ -88,6 +88,21 @@ SITES = {
             "description": ".detail-description",
         },
     },
+    "CSU_Dominguez_Hills": {
+        "name": "CSU Dominguez Hills",
+        "type": "courseleaf",
+        "subjectCodesUrl": "https://catalog.csudh.edu/courses/",
+        "selectors": {
+            "code": lambda el: el.select_one(".detail-code").text.rstrip("."),
+            "title": lambda el: el.select_one(".detail-title").text.rstrip("."),
+            # The last .courseblockextra element is always "Offered ...", but sometimes
+            #  it's the only one; take a description only if there's more than one
+            "description": lambda el: (
+                courseblockextra := el.select(".courseblockextra"),
+                "" if not len(courseblockextra) > 1 else courseblockextra[0].text,
+            )[-1],
+        },
+    },
     "CSU_East_Bay": {
         "name": "CSU East Bay",
         "type": "moderncampus",
