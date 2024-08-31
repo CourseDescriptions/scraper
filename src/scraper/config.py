db64ab40839beb2ec88b2c67d85e9c15700e8325
Simon Wiles <simonjwiles@gmail.com>
2024-08-31 09:23:42 -0700

    Add support for CSU Chico

--- src/scraper/config.py
+++ src/scraper/config.py
@@ -78,6 +78,16 @@ SITES = {
             ),
         },
     },
+    "CSU_Chico": {
+        "name": "CSU Chico",
+        "type": "courseleaf",
+        "subjectCodesUrl": "https://catalog.csuchico.edu/courses/",
+        "selectors": {
+            "code": ".detail-code",
+            "title": ".detail-title",
+            "description": ".detail-description",
+        },
+    },
     "CSU_East_Bay": {
         "name": "CSU East Bay",
         "type": "moderncampus",
