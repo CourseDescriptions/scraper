8071366f5fb0a11df79858f2ce773ce344d66bc3
Rohan Parekh <rohan@Rohans-MacBook-Pro.local>
2025-02-14 10:55:24 -0800

    fixed split to be on weird chars website uses

--- src/config/CSU_East_Bay.py
+++ src/config/CSU_East_Bay.py
@@ -3,10 +3,10 @@ def get_config(): return {
     "type": "moderncampus",
     "startUrl": "https://catalog.csueastbay.edu/content.php?catoid=35&navoid=30996",
     "selectors": {
-        "code": lambda el: el.select_one("#course_preview_title").text.split(" - ")[
+        "code": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
             0
         ],
-        "title": lambda el: el.select_one("#course_preview_title").text.split(" - ")[
+        "title": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
             1
         ],
     }
