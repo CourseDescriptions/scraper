f2867205198bf29b4a41a690622bba7eb0c9e00b
rohan4191 <rohan.parekh419@gmail.com>
2025-02-23 11:24:23 -0800

    First try at cal poly humboldt

---
+++ src/config/Cal_Poly_Humboldt.py
@@ -0,0 +1,13 @@
+def get_config(): return {
+  "name": "Cal Poly Humboldt",
+  "type": "moderncampus",
+  "startUrl": "https://catalog.humboldt.edu/content.php?catoid=10&navoid=1555",
+  "selectors": {
+      "code": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
+          0
+      ],
+      "title": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
+          1
+      ],
+  }
+}
