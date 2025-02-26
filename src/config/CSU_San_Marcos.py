e43f500b5fbde5ed665ae9011f1303a89b7c3d9a
Rohan <rohan.parekh419@gmail.com>
2025-02-26 11:17:30 -0800

    csu san marcos getting ConnectionError on request.get

---
+++ src/config/CSU_San_Marcos.py
@@ -0,0 +1,13 @@
+def get_config(): return {
+    "name": "CSU San Marcos",
+    "type": "moderncampus",
+    "startUrl": "https://catalog.csusm.edu/content.php?catoid=10&navoid=1811",
+    "selectors": {
+        "code": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
+            0
+        ],
+        "title": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
+            1
+        ],
+    }
+}
