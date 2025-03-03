7a32c9c66de7080531a9ee27b527130caeb6d933
rohan4191 <rohan.parekh419@gmail.com>
2025-03-03 12:10:43 -0800

    san bernadino attempt to filter out non description stuff

--- src/config/CSU_San_Bernadino.py
+++ src/config/CSU_San_Bernadino.py
@@ -5,6 +5,8 @@ def get_config(): return {
   "selectors": {
       "code": lambda el: el.select_one(".coursetitle").text.split(". ")[0],
       "title": lambda el: el.select_one(".coursetitle").text.split(". ")[1],
-      "description": ".courseblockdesc",
+      "description": lambda el: el.select_one(".courseblockdesc").text,
+      # "description": lambda el: "".join([line for line in el.select_one(".courseblockdesc").get_text(strip=True, separator="\n").splitlines()
+      #                            if "Prerequisite:" not in line and "Corequisite:" not in line]),
   }
 }
