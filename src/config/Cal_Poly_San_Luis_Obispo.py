e58b5e56d00640b6d1f1a3e5bf46eb64f2385247
Rohan <rohan.parekh419@gmail.com>
2025-03-06 13:03:17 -0800

    Switched cal poly to courseleaf cause thats what it is

--- src/config/Cal_Poly_San_Luis_Obispo.py
+++ src/config/Cal_Poly_San_Luis_Obispo.py
@@ -1,13 +1,10 @@
 def get_config(): return {
     "name": "Cal Poly San Luis Obispo",
-    "type": "moderncampus",
-    "startUrl": "https://catalog.calpoly.edu/coursesaz/",
+    "type": "courseleaf",
+    "subjectCodesUrl": "https://catalog.calpoly.edu/coursesaz/",
     "selectors": {
-        "code": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
-            0
-        ],
-        "title": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
-            1
-        ],
-    }
+      "code": ".detail-code",
+      "title": ".detail-title",
+      "description": ".courseblockextra:not(.noindent)",
+  }
 }
