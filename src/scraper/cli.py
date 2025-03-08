f63f9fe828e30e70b517ddaaec00de79f7da2e98
Rohan <rohan.parekh419@gmail.com>
2025-03-08 11:06:52 -0800

    cleaned up comments

--- src/scraper/cli.py
+++ src/scraper/cli.py
@@ -98,19 +98,10 @@ def get(
 
     site_config = SITES[site_id]
 
-    # use specified scraper type
+    # use specified scraper type on site's config dict
     scraper = site_config["type"](site_config)
-    # if site_config["type"] == "courseleaf":
-    #     scraper = CourseLeafScraper(site_config)
-    # elif site_config["type"] == "moderncampus":
-    #     scraper = ModernCampusScraper(site_config)
-    # elif site_config["type"] == "ucla":
-    #     scraper = UclaScraper(site_config)
-    # elif site_config["type"] == "ucsc":
-    #     scraper = UcscScraper(site_config)
-    # else:
-    #     logging.fatal('Scraper type "%s" not supported.', site_config["type"])
-    #     raise typer.Abort()
+    # logging.fatal('Scraper type "%s" not supported.', site_config["type"])
+    # raise typer.Abort()
 
     # scrape the data (use flip of noCache as useCache arg)
     data = scraper.get(not noCache, limit)
