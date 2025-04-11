1b913f7f1c27e180c3e18665fdc3d65ae30960a5
rohan4191 <rohan.parekh419@gmail.com>
2025-04-11 11:46:22 -0700

    UIUC scraper working but getting a little too much extraneous stuff at the end

---
+++ src/config/UIUC.py
@@ -0,0 +1,12 @@
+from scraper.courseleaf import CourseLeafScraper
+
+def get_config(): return {
+    "name": "University of Illinois Urbana-Champaign",
+    "type": CourseLeafScraper,
+    "subjectCodesUrl": "http://catalog.illinois.edu/courses-of-instruction/",
+    "selectors": {
+      "code": lambda el : el.select_one(".courseblocktitle").text.split(" \u2002 ")[0],
+      "title": lambda el : el.select_one(".courseblocktitle").text.split(" \u2002 ")[1],
+      "description": ".courseblockdesc",
+  }
+}
