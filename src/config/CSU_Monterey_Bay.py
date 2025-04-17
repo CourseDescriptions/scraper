f4a7a96784e6b5c267176b55518c02751114e08d
Rohan <rohan.parekh419@gmail.com>
2025-04-17 10:51:53 -0700

    use normalize text, no longer trip up on that one empty course thanks to data extract better recovery

--- src/config/CSU_Monterey_Bay.py
+++ src/config/CSU_Monterey_Bay.py
@@ -1,14 +1,15 @@
 from scraper.moderncampus import ModernCampusScraper
+from scraper.common import normalize_text
 
 def get_config(): return {
     "name": "CSU Monterey Bay",
     "type": ModernCampusScraper,
     "startUrl": "https://catalog.csumb.edu/content.php?catoid=10&navoid=549",
     "selectors": {
-        "code": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
+        "code": lambda el: normalize_text(el.select_one("#course_preview_title").text).split(" - ")[
             0
         ],
-        "title": lambda el: el.select_one("#course_preview_title").text.split("\xa0-\xa0")[
+        "title": lambda el: normalize_text(el.select_one("#course_preview_title").text).split(" - ")[
             1
         ],
     }
