aa622a661f0edcd8d63ca6d95cc949727f36b6a5
Gabi Keane <gabakeane@gmail.com>
2025-09-25 15:53:58 -0700

    update catalog home url

--- src/config/Cal_Poly_Humboldt.py
+++ src/config/Cal_Poly_Humboldt.py
@@ -4,7 +4,7 @@ from scraper.common import normalize_text
 def get_config(): return {
   "name": "Cal Poly Humboldt",
   "type": ModernCampusScraper,
-  "startUrl": "https://catalog.humboldt.edu/content.php?catoid=10&navoid=1555",
+  "startUrl": "https://catalog.humboldt.edu/content.php?catoid=14&navoid=2303",
   "selectors": {
       "code": lambda el: normalize_text(el.select_one("#course_preview_title").text).split(" - ")[0],
       "title": lambda el: normalize_text(el.select_one("#course_preview_title").text).split(" - ")[1],
